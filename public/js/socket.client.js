6e03115637f8c7fd41ccfcdf4c4d50ea820dab8b
Angel Travieso <angeltraviesoc@gmail.com>
2023-01-30 10:25:47 -0400

    mensajes de conexion y desconexion - Cliente

--- public/js/socket.client.js
+++ public/js/socket.client.js
@@ -1,3 +1,26 @@
+// Variables HTML
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
 
 // Iniciar socket (cliente)
 const socket = io();
+
+// On para escuchar eventos
+socket.on('connect', () => { // connect cuando se conecta al socket
+    console.log('Conectado');
+
+    // Mostrar span 'Online'
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+
+});
+
+socket.on('disconnect', () => { // disconnect cuando se conecta al socket
+    console.log('Desconectado del servidor');
+
+    // Mostrar span 'Offline'
+    lblOnline.style.display = 'none';
+    lblOffline.style.display = '';
+
+});
